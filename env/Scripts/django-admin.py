15f61746db6aff32575de8177f0019cf222931e2
Thomas <thomas.savelli92@gmail.com>
2024-02-05 18:36:31 +0100

    Suppression de env et update de .gitignore

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\thoma\Desktop\OC-LETTINGS-V2\Python-OC-Lettings-FR\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
